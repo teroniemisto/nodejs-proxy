b3a8c5eeaa4215c849fae29b7fe8d86c780253f6
teroniemisto <tero.niemisto@digia.com>
2015-03-09 13:24:36 +0200

    initial
    testing

---
+++ proxy.js
@@ -0,0 +1,14 @@
+var port = process.env.PORT || 8080;
+
+var httpProxy = require('http-proxy');
+var express = require('express');
+
+var app = express();
+var apiProxy = httpProxy.createProxyServer();
+
+app.get("/*", function(req, res){ 
+  apiProxy.web(req, res, { target: 'http://google.com' });
+});
+
+app.listen(port);
+console.log("Proxy Server started! Open http://localhost:" + port + " in your web browser to test locally.");
